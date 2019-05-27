baedb4fc83abbf1199f17e3a50a07b5cec771722
Gijs Huitenga <27202@ma-web.nl>
2019-05-27 22:25:49 +0200

    Add files via upload

---
+++ public/head-foot/footer.php
@@ -0,0 +1,28 @@
+
+		<footer>
+
+			<div class="footer-content">
+
+				<div class="left-content">
+					<ul>
+						<li><a href="https://www.linkedin.com/in/gijs-huitenga-2692b214a/" target="_blank">LinkedIn</a></li>
+						<li><a href="https://github.com/GijsHuitenga" target="_blank">Github</a></li>
+						<li><a href="https://www.instagram.com/gs_h4/" target="_blank">Instagram</a></li>
+						<li><a href="https://www.facebook.com/gijs.huitenga" target="_blank">Facebook</a></li>
+					</ul>
+				</div>
+
+				<div id="vert-line"></div>
+
+				<div class="right-content">
+					<p>&copy; Gijs Huitenga, 2019</p>
+					<a href="http://gijs-huitenga.nl/index.html" target="_blank">www.gijs-huitenga.nl</a><br><br>
+					<a href="contact.php">Contact</a>
+				</div>
+
+			</div>
+
+		</footer>
+	</body>
+
+</html>
---
+++ public/head-foot/header.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Gijs Huitenga">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="js/script.js"></script>
+		<title>F1-local | Gijs Huitenga</title>
+	</head>
+
+	<body>
+
+		<header id="top">
+			<h1 class="logo"><a href="#top">F1-LOCAL</a></h1>
+			<nav>
+				<a href="#about">About Us</a>
+				<a href="#agenda">F1 Agenda</a>
+				<a href="#news">F1 News</a>
+			</nav>	
+		</header>
